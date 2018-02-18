ee9ea73040c22aa4833f3d10a1bd9502eebdd72a
taobear <1743909352@qq.com>
2018-02-19 00:01:26 +0800

    initial commit

---
+++ Json.h
@@ -0,0 +1,40 @@
+#ifndef __JSONPARSER_JSON_H_
+#define __JSONPARSER_JSON_H_
+#include <string>
+
+namespace JsonParser
+{
+
+enum Json_type {
+	JSON_NULL,
+	JSON_FALSE,
+	JSON_TRUE,
+	JSON_NUMBER,
+	JSON_STRING,
+	JSON_ARRAY,
+	JSON_OBJECT
+};
+
+enum Json_state {
+	OK = 0,
+	EXPECT_VALUE,
+	INVALID_VALUE,
+	ROOT_NOT_SINGULAR
+};
+
+struct Json_value {
+	Json_type type;
+};
+
+class Json
+{
+public:
+	Json();
+	~Json();
+
+	Json_state parse(Json_value* jv, const std::string& json_str);
+};
+
+} // end of JsonParser
+
+#endif // __JSONPARSER_JSON_H_
---
+++ test.cpp
@@ -0,0 +1,62 @@
+#include "Json.h"
+using namespace JsonParser;
+
+static int main_ret = 0;
+static int test_count = 0;
+static int test_pass = 0;
+
+#define EXPECT_EQ_BASE(equality, expect, actual, format) \
+     do { \
+          test_count++; \
+          if (equality) \
+              test_pass++; \
+          else { \
+          	  fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", \
+          	  	  __FILE__, __LINE__, expect, actual); \
+          	  main_ret = 1; \
+          } \
+     } while (0)
+
+#define EXPECT_EQ_INT(expect, actual) \
+    EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+
+static void test_parse_null() 
+{
+	Json       js;
+	Json_value val;
+
+	EXPECT_EQ_INT(js.parse(&val, "null"), Json_state::OK);
+	EXPECT_EQ_INT(val.type, Json_type::JSON_NULL);
+}
+
+static void test_parse_false()
+{
+	Json       js;
+	Json_value val;
+
+	EXPECT_EQ_INT(js.parse(&val, "false"), Json_state::OK);
+	EXPECT_EQ_INT(val.type, Json_type::JSON_FALSE);
+}
+
+static void test_parse_true()
+{
+	Json       js;
+	Json_value val;
+
+	EXPECT_EQ_INT(js.parse(&val, "true"), Json_state::OK);
+	EXPECT_EQ_INT(val.type, Json_type::JSON_TRUE);
+}
+
+static void test_parse()
+{
+	test_parse_null();
+	test_parse_false();
+	test_parse_true();
+}
+
+int main(int argc, char **argv)
+{
+	test_parse();
+    printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+    return main_ret;
+}
